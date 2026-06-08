64ca3f8688eccafaaedf5bd4530672b884fe1502
oscarlo-lgtm <jisoeeem@gmail.com>
2026-06-08 16:48:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ def login():
         cur.close(); conn.close()
         
         if usuario:
-            session['vendedor'] = usuario['nombre']
-            return redirect(url_for('inicio'))
+            session['vendedor'] = f"{usuario['nombre']} {usuario['ap_materno']}"
+            return redirect('/inicio')
         return "Usuario o contraseña incorrectos"
     return render_template('login.html')
 
 @app.route('/inicio')
 def inicio():
-    if 'vendedor' not in session: return redirect(url_for('login'))
+    if 'vendedor' not in session: return redirect('/')
     return render_template('pos.html')
 
 @app.route('/registrar/<token>', methods=['GET', 'POST'])
@@ -36,11 +36,13 @@ def registrar(token):
     if request.method == 'POST':
         conn = get_db()
         cur = conn.cursor()
-        cur.execute("INSERT INTO usuarios (nombre, ap_paterno, ap_materno, celular, password) VALUES (%s, %s, %s, %s, %s)",
-                    (request.form['nombre'], request.form['ap_paterno'], request.form['ap_materno'], request.form['celular'], request.form['password']))
+        # Creamos fecha uniendo los campos del formulario
+        fecha = f"{request.form['anio']}-{request.form['mes']}-{request.form['dia']}"
+        cur.execute("INSERT INTO usuarios (nombre, ap_paterno, ap_materno, fecha_nac, celular, password) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (request.form['nombre'], request.form['ap_paterno'], request.form['ap_materno'], fecha, request.form['celular'], request.form['password']))
         conn.commit()
         cur.close(); conn.close()
-        return redirect(url_for('login'))
+        return redirect('/')
     return render_template('registro.html')
 
 if __name__ == '__main__':
